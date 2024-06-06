5b743a35335494bfe3766b1633d2bc84c402131f
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-06 14:04:41 -0400

    Set up outlet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <>
+      <h1>Project Client</h1>;
+      <Outlet />
+    </>
+  )  
 }
 
 export default App;
