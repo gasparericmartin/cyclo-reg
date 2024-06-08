b075f7a0a71129bb1bc56219e54a0f9d052b0302
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-08 13:16:16 -0400

    Create xml structure for RaceCard

--- client/src/components/RaceCard.js
+++ client/src/components/RaceCard.js
@@ -1,7 +1,19 @@
 
 
-function RaceCard() {
-
+function RaceCard({race}) {
+    const {id, name, date, location, 
+            length, registration_fee} = race 
+        
+        
+    return (
+        <>
+            <h1>{name}</h1>
+            <p>{date}</p>
+            <p>{location}</p>
+            <p>{length} KMs</p>
+            <p>{registration_fee}</p>
+        </>
+    )
 }
 
 export default RaceCard
