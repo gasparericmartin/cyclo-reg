8244a7d982f8625da51667c787be5f8157e742ae
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-08 15:31:03 -0400

    Create component structure

--- client/src/components/RaceInfo.js
+++ client/src/components/RaceInfo.js
@@ -1,7 +1,39 @@
 
 
-function RaceInfo() {
+function RaceInfo({race}) {
+    const {
+        id,
+        name,
+        date,
+        location,
+        length,
+        registration_fee,
+        registrations
+        } = race   
+
+    return (
+        <>
+            <h1>{name}</h1>
+            <p>{date}</p>
+            <p>{location}</p>
+            <p>{length}</p>
+            <p>${registration_fee}</p>
+    
+            {registrations.map((registration) => {
+                return (
+                    <>
+                        <h2>{registration.cyclist.name}</h2>
+                        <p>{registration.bike}</p>
+                    </>
+                )
+                    
+
+            })}
+        </>
+    )
 
 }
+
+
 
 export default RaceInfo
