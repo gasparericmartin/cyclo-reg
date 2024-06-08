7582847a11d400bcd0eb8739e89a4a51d0913984
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-08 13:21:29 -0400

    Render race data

--- client/src/pages/Races.js
+++ client/src/pages/Races.js
@@ -1,8 +1,24 @@
-
+import {useState, useEffect} from 'react'
+import RaceCard from '../components/RaceCard'
 
 
 function Races() {
-    return <h1>Races</h1>
+    const [races, setRaces] = useState([])
+    
+    useEffect(() => {
+        fetch('http://localhost:5555/races')
+        .then(r => r.json())
+        .then(data => setRaces(data))
+    }, [])
+    
+    return (
+        <>
+            <h1>Races</h1>
+            {races.map((race) => {
+                return <RaceCard key={race.id} race={race} />
+            })}
+        </>
+    )
 }
 
 export default Races
