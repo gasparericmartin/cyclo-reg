f330e9193c3afed39b16b7d27df13790176c895e
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-12 18:55:18 -0400

    Create seed data with faker

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
+from random import randint, uniform, choice as rc
 import datetime
 
 # Remote library imports
@@ -11,69 +11,81 @@ from faker import Faker
 from app import app
 from models import db, Cyclist, Race, Registration
 
+fake = Faker()
+
+bike_brands = ['Kona', 'All-City', 'Specialized', 'Jones',
+               'Sklar', 'Cotik', 'Otso', 'Moots', 'Litespeed',
+               'Cannondale']
+
+def create_cyclists():
+    cyclists = []
+
+    for x in range(10):
+        cyclist = Cyclist(
+                    name = fake.name(),
+                    age = randint(10, 70),
+                    hometown = fake.city()
+        )
+        cyclists.append(cyclist)
+    
+    return cyclists
+
+def create_races():
+    races = []
+
+    for x in range(10):
+        race = Race(
+            name = fake.company(),
+            date = fake.date_time(),
+            location = fake.city(),
+            length = randint(20, 150),
+            registration_fee = uniform(0.0, 99.99)
+        )    
+        races.append(race)
+    
+    return races
+
+def create_registrations():
+    registrations = []
+
+    for x in range(10):
+        registration = Registration(
+                    bike = rc(bike_brands),
+                    cyclist_id = x,
+                    race_id = randint(1, 10)
+        )
+        registrations.append(registration)
+    
+    return registrations
+
+
+
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        print('Clearing db...')
         Cyclist.query.delete()
         Race.query.delete()
         Registration.query.delete()
         
-        print("Starting seed...")
-
-        cyclist1 = Cyclist(
-                    name = 'Janice',
-                    age = 56,
-                    hometown = 'New York, NY'
-        )
-        cyclist2 = Cyclist(
-                    name = 'Dillon',
-                    age = 23,
-                    hometown = 'Portland, ME'
-        )
-
-        db.session.add(cyclist1)
-        db.session.add(cyclist2)
+        print('Seeding cyclists...')
+        cyclists = create_cyclists()
+        db.session.add_all(cyclists)
         db.session.commit()
 
-        print('Cyclists seeded')
-
-        race_1 = Race(
-                    name = 'Badlands 500',
-                    date = datetime.datetime.now(),
-                    location = 'Arizona',
-                    length = 36,
-                    registration_fee = 100.0
-        )    
-        race_2 = Race(
-                    name = 'Nutmeg',
-                    date = datetime.datetime.now(),
-                    location = 'New Haven, CT',
-                    length = 20.0,
-                    registration_fee = 200.0
-        )
-
-        db.session.add(race_1)
-        db.session.add(race_2)
+        print('Seeding races...')
+        races = create_races()
+        db.session.add_all(races)
         db.session.commit()
 
-        print('Races seeded')
-
-        reg_1 = Registration(
-                    bike = 'Kona',
-                    cyclist_id = 1,
-                    race_id = 1
-        )
-        reg_2 = Registration(
-                    bike = 'All-City',
-                    cyclist_id = 2,
-                    race_id = 2
-        )
-
-        db.session.add(reg_1)
-        db.session.add(reg_2)
+        print('Seeding registrations...')
+        registrations = create_registrations()
+        db.session.add_all(registrations)
         db.session.commit()
 
-        print('Registrations seeded')
+        print('Seed complete.')
+
 
 
         
