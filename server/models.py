d2084b01cfed5ebf5e94f5bb6c4be39169ed1403
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-06-20 14:07:11 -0400

    Comment out unused imports

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import validates
+# from sqlalchemy.ext.hybrid import hybrid_property
+# from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 from config import db
